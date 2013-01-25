24bc84d168405664d45ca24e523a8b6350f138b3
Jeremy Stephens <viking@pillageandplunder.net>
2013-01-25 16:31:33 -0600

    Include Enumerable in Source, add #field_names and #all

--- lib/ethel/source.rb
+++ lib/ethel/source.rb
@@ -1,5 +1,22 @@
 module Ethel
   class Source
+    include Enumerable
+
+    def schema
+      raise NotImplementedError
+    end
+
+    def each
+      raise NotImplementedError
+    end
+
+    def field_names
+      schema.collect(&:first)
+    end
+
+    def all
+      to_a
+    end
   end
 end
 
--- test/unit/test_source.rb
+++ test/unit/test_source.rb
@@ -1,4 +1,45 @@
 require 'helper'
 
 class TestSource < Test::Unit::TestCase
+  def new_subclass(&block)
+    Class.new(Ethel::Source, &block)
+  end
+
+  test "schema raises NotImplementedError" do
+    klass = new_subclass
+    source = klass.new
+    assert_raises(NotImplementedError) { source.schema }
+  end
+
+  test "field_names" do
+    klass = new_subclass do
+      def schema
+        [['foo', {}], ['bar', {}]]
+      end
+    end
+    source = klass.new
+    assert_equal %w{foo bar}, source.field_names
+  end
+
+  test "each raises NotImplementedError" do
+    klass = new_subclass
+    source = klass.new
+    assert_raises(NotImplementedError) { source.each }
+  end
+
+  test "includes Enumerable" do
+    assert_include Ethel::Source.included_modules, Enumerable
+  end
+
+  test "all" do
+    klass = new_subclass do
+      def each
+        yield({'foo' => 1, 'bar' => 2})
+        yield({'foo' => 3, 'bar' => 4})
+      end
+    end
+    source = klass.new
+    assert_equal [{'foo' => 1, 'bar' => 2}, {'foo' => 3, 'bar' => 4}],
+      source.all
+  end
 end
