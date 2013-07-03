f2b4dfd476b0adbcb1e40de902a7ecfcb4aa3ef0
Jeremy Stephens <jeremy.f.stephens@vanderbilt.edu>
2013-07-03 17:59:01 -0500

    Fully perform each operation in sequence
    - use intermediary Memory readers/writers for migrations involving more
      than one operation
    - handle empty migrations (for simple conversions)

--- lib/ethel/migration.rb
+++ lib/ethel/migration.rb
@@ -10,13 +10,42 @@ module Ethel
     end
 
     def run
-      @writer.prepare(@dataset)
+      if @operations.empty?
+        @writer.prepare(@dataset)
+        @reader.each_row do |row|
+          @writer.add_row(row)
+        end
+        @writer.flush
+      else
+        # If there is only one operation, we read directly from the
+        # reader and write directly to the writer. However, if there
+        # is more than one operation, intermediary memory
+        # readers/writers are used.
+        reader = @reader
+        last = @operations.length - 1
+        0.upto(last) do |i|
+          op = @operations[i]
+          op.setup(@dataset)
 
-      @reader.each_row do |row|
-        row = @operations.inject(row) { |r, op| op.transform(r) }
-        @writer.add_row(row)
+          writer =
+            if i == last
+              @writer
+            else
+              Writers::Memory.new
+            end
+          writer.prepare(@dataset)
+
+          reader.each_row do |row|
+            row = op.transform(row)
+            writer.add_row(row)
+          end
+          writer.flush
+
+          if i < last
+            reader = Readers::Memory.new(writer.data)
+          end
+        end
       end
-      @writer.flush
     end
 
     def method_missing(name, *args, &block)
@@ -32,7 +61,16 @@ module Ethel
     protected
 
     def add_operation(op)
-      op.setup(@dataset)
+      # FIXME: We need an immediate check to see if the operation
+      # is valid for the current state of the dataset. Also, is
+      # the transformation valid? We could run some test data
+      # through it and find out. This way the migration doesn't
+      # die after an expensive operation has already taken place.
+      # An other option is to have wrap operations in
+      # "transactions". If an operation fails, we can save the
+      # state of the data in memory or something and the migration
+      # can be resumed when the user has fixed the problem.
+
       @operations << op
     end
   end
--- test/unit/ethel/test_migration.rb
+++ test/unit/ethel/test_migration.rb
@@ -8,6 +8,21 @@ class TestMigration < Test::Unit::TestCase
     Ethel::Operations::Update.stubs(:new)
   end
 
+  test "simple conversion (no operations)" do
+    dataset = stub('dataset')
+    Ethel::Dataset.expects(:new).returns(dataset)
+    @reader.expects(:read).with(dataset)
+    m = Ethel::Migration.new(@reader, @writer)
+
+    seq = SequenceHelper.new('run sequence')
+    seq << @writer.expects(:prepare).with(dataset)
+    row = stub('row')
+    seq << @reader.expects(:each_row).yields(row)
+    seq << @writer.expects(:add_row).with(row)
+    seq << @writer.expects(:flush)
+    m.run
+  end
+
   test "casting a field" do
     dataset = stub('dataset')
     Ethel::Dataset.expects(:new).returns(dataset)
@@ -17,17 +32,61 @@ class TestMigration < Test::Unit::TestCase
     cast_operation = stub('cast operation')
     Ethel::Operations::Cast.expects(:new).with('foo', :integer).
       returns(cast_operation)
-    cast_operation.expects(:setup).with(dataset)
     m.cast('foo', :integer)
 
     seq = SequenceHelper.new('run sequence')
-
+    seq << cast_operation.expects(:setup).with(dataset)
     seq << @writer.expects(:prepare).with(dataset)
     row = stub('row')
     seq << @reader.expects(:each_row).yields(row)
     seq << cast_operation.expects(:transform).with(row).returns(row)
     seq << @writer.expects(:add_row).with(row)
     seq << @writer.expects(:flush)
+    m.run
+  end
+
+  test "multi-operation migration" do
+    dataset = stub('dataset')
+    Ethel::Dataset.expects(:new).returns(dataset)
+    @reader.expects(:read).with(dataset)
+    m = Ethel::Migration.new(@reader, @writer)
+
+    cast_operation_1 = stub('cast operation 1')
+    Ethel::Operations::Cast.expects(:new).with('foo', :integer).
+      returns(cast_operation_1)
+    m.cast('foo', :integer)
+
+    cast_operation_2 = stub('cast operation 2')
+    Ethel::Operations::Cast.expects(:new).with('bar', :integer).
+      returns(cast_operation_2)
+    m.cast('bar', :integer)
+
+    seq = SequenceHelper.new('run sequence')
+    memwriter = stub('memory writer')
+    write_data = stub('write data')
+    memreader = stub('memory reader')
+    row = stub('row')
+    new_row = stub('new row')
+
+    # round 1
+    seq << cast_operation_1.expects(:setup).with(dataset)
+    seq << Ethel::Writers::Memory.expects(:new).returns(memwriter)
+    seq << memwriter.expects(:prepare).with(dataset)
+    seq << @reader.expects(:each_row).yields(row)
+    seq << cast_operation_1.expects(:transform).with(row).returns(new_row)
+    seq << memwriter.expects(:add_row).with(new_row)
+    seq << memwriter.expects(:flush)
+
+    # round 2
+    seq << memwriter.expects(:data).returns(write_data)
+    seq << Ethel::Readers::Memory.expects(:new).with(write_data).returns(memreader)
+    seq << cast_operation_2.expects(:setup).with(dataset)
+    seq << @writer.expects(:prepare).with(dataset)
+    seq << memreader.expects(:each_row).yields(row)
+    seq << cast_operation_2.expects(:transform).with(row).returns(new_row)
+    seq << @writer.expects(:add_row).with(new_row)
+    seq << @writer.expects(:flush)
+
     m.run
   end
 
