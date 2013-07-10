6ba76760d81bf127d656ed2f4830eaffb90916f2
Jeremy Stephens <jeremy.f.stephens@vanderbilt.edu>
2013-07-10 12:53:54 -0500

    Add Rename operation

---
+++ lib/ethel/operations/rename.rb
@@ -0,0 +1,26 @@
+module Ethel
+  module Operations
+    class Rename < Operation
+      def initialize(name, new_name)
+        super
+        @name = name
+        @new_name = new_name
+      end
+
+      def setup(dataset)
+        super
+        field = dataset.field(@name)
+        new_field = Field.new(@new_name, :type => field.type)
+        dataset.alter_field(@name, new_field)
+      end
+
+      def transform(row)
+        row = super(row)
+        row[@new_name] = row.delete(@name)
+        row
+      end
+
+      register('rename', self)
+    end
+  end
+end
---
+++ test/unit/ethel/operations/test_rename.rb
@@ -0,0 +1,37 @@
+require 'helper'
+
+module TestOperations
+  class TestRename < Test::Unit::TestCase
+    include ConstantsHelper
+
+    def setup
+      @field = stub('original field', :name => 'foo', :type => :string)
+      @dataset = stub('dataset', :field => @field)
+      @new_field = stub('new field', :name => 'bar')
+      Field.stubs(:new).returns(@new_field)
+    end
+
+    test "subclass of Operation" do
+      assert_equal Operation, Operations::Rename.superclass
+    end
+
+    test "alters field during setup callback" do
+      op = Operations::Rename.new('foo', 'bar')
+
+      @dataset.expects(:field).with('foo').returns(@field)
+      Field.expects(:new).with('bar', :type => :string).returns(@new_field)
+      @dataset.expects(:alter_field).with('foo', @new_field)
+      op.setup(@dataset)
+    end
+
+    test "renames field during transform" do
+      row = {'foo' => 123}
+      op = Operations::Rename.new('foo', 'bar')
+      assert_equal({'bar' => 123}, op.transform(row))
+    end
+
+    test "registers itself" do
+      assert_equal Operations::Rename, Operation.operation('rename')
+    end
+  end
+end
