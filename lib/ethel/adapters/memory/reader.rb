ee71880388a671825aec70f6092cdfc674add9d3
Jeremy Stephens <viking@pillageandplunder.net>
2013-08-22 14:10:37 -0500

    Handle nils better in Memory::Reader, allow specifying field types

--- lib/ethel/adapters/memory/reader.rb
+++ lib/ethel/adapters/memory/reader.rb
@@ -4,29 +4,46 @@ module Ethel
       class Reader < ::Ethel::Reader
         def initialize(options)
           @data = options[:data]
+          @field_types = options[:field_types]
         end
 
         def read(dataset)
-          fields = {}
+          if @field_types
+            field_types = @field_types
+            autodiscover = false
+          else
+            field_types = {}
+            autodiscover = true
+          end
+
           @data.to_enum.with_index do |row, i|
-            if i == 0
+            if i == 0 && autodiscover
               row.each do |(name, val)|
-                fields[name] = Util.type_of(val)
+                field_types[name] = Util.type_of(val)
               end
             else
               row.each do |(name, val)|
-                if !fields.has_key?(name)
+                if !field_types.has_key?(name)
                   raise InvalidFieldName, "row #{i}: #{name} is not a valid field"
                 end
+                next if val.nil?
 
                 type = Util.type_of(val)
-                if type != fields[name]
-                  raise InvalidFieldType, "row #{i}: expected #{name} to be of type #{fields[name]}, but was #{type}"
+                if field_types[name].nil? && autodiscover
+                  if type
+                    field_types[name] = type
+                  end
+                elsif type != field_types[name]
+                  raise InvalidFieldType, "row #{i}: expected #{name} to be of type #{field_types[name].inspect}, but was #{type.inspect}"
                 end
               end
             end
           end
-          fields.each_pair do |name, type|
+
+          field_types.each_pair do |name, type|
+            if autodiscover && type.nil?
+              raise InvalidFieldType, "column #{name.inspect} had only nil values, could not determine type"
+            end
             field = Field.new(name, :type => type)
             dataset.add_field(field)
           end
--- test/unit/ethel/adapters/memory/test_reader.rb
+++ test/unit/ethel/adapters/memory/test_reader.rb
@@ -38,6 +38,59 @@ module TestAdapters
         end
       end
 
+      test "#read with nil value first" do
+        reader = Memory::Reader.new(:data => [{'foo' => nil}, {'foo' => 456}])
+
+        dataset = stub('dataset')
+        field = stub('field')
+        Field.expects(:new).with('foo', :type => :integer).returns(field)
+        dataset.expects(:add_field).with(field)
+        reader.read(dataset)
+      end
+
+      test "#read with nil value second" do
+        reader = Memory::Reader.new(:data => [{'foo' => 456}, {'foo' => nil}])
+
+        dataset = stub('dataset')
+        field = stub('field')
+        Field.expects(:new).with('foo', :type => :integer).returns(field)
+        dataset.expects(:add_field).with(field)
+        reader.read(dataset)
+      end
+
+      test "#read with all nil values" do
+        reader = Memory::Reader.new(:data => [{'foo' => nil}, {'foo' => nil}])
+        dataset = stub('dataset')
+        assert_raises(InvalidFieldType) do
+          reader.read(dataset)
+        end
+      end
+
+      test "#read with manually specified field types" do
+        reader = Memory::Reader.new({
+          :data => [{'foo' => nil}, {'foo' => nil}],
+          :field_types => {'foo' => :integer}
+        })
+
+        dataset = stub('dataset')
+        field = stub('field')
+        Field.expects(:new).with('foo', :type => :integer).returns(field)
+        dataset.expects(:add_field).with(field)
+        reader.read(dataset)
+      end
+
+      test "#read with manually specified field types and inconsistent names" do
+        reader = Memory::Reader.new({
+          :data => [{'foo' => 123}, {'bar' => 456}],
+          :field_types => {'foo' => :integer}
+        })
+
+        dataset = stub('dataset')
+        assert_raises(InvalidFieldName) do
+          reader.read(dataset)
+        end
+      end
+
       test "#each_row" do
         reader = Memory::Reader.new(:data => [{'foo' => 123}, {'foo' => 456}])
         reader.to_enum(:each_row).with_index do |row, i|
