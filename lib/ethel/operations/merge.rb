a0380aaab7bd573f2417bfc556d660ef0d1fdf24
Jeremy Stephens <jeremy.f.stephens@vumc.org>
2019-01-25 11:13:50 -0600

    Add rudimentary join-table support to merge operation

--- lib/ethel/operations/merge.rb
+++ lib/ethel/operations/merge.rb
@@ -5,7 +5,7 @@ module Ethel
         super
         @target_reader = target_reader
 
-        origin_fields = target_fields = nil
+        origin_fields = target_fields = []
         if options.has_key?(:fields)
           origin_fields = target_fields = options[:fields]
         elsif options.has_key?(:origin_fields)
@@ -17,38 +17,99 @@ module Ethel
           end
         end
 
-        @origin_fields = origin_fields.is_a?(Array) ? origin_fields : [origin_fields]
-        @target_fields = target_fields.is_a?(Array) ? target_fields : [target_fields]
-        if @origin_fields.length != @target_fields.length
+        if origin_fields.empty? || target_fields.empty?
+          raise "origin and target fields must be specified"
+        end
+
+        origin_fields = origin_fields.is_a?(Array) ? origin_fields : [origin_fields]
+        target_fields = target_fields.is_a?(Array) ? target_fields : [target_fields]
+        if origin_fields.length != target_fields.length
           raise ArgumentError, "origin and target fields must be the same length"
+        end
+
+        @origin_fields = origin_fields.collect do |field|
+          field = process_field(field)
+          if field[:name] != field[:alias]
+            rename = Operation["rename"].new(field[:name], field[:alias])
+            add_pre_operation(rename)
+          end
+          field
+        end
+        @target_fields = target_fields.collect do |field|
+          field = process_field(field)
+          if field[:name] != field[:alias]
+            rename = Operation["rename"].new(field[:name], field[:alias])
+            add_post_operation(rename)
+          end
+          field
+        end
+
+        if options.has_key?(:join_reader)
+          @joins = {}
+          origin_names = @origin_fields.collect { |f| f[:alias] }
+          target_names = @target_fields.collect { |f| f[:alias] }
+          options[:join_reader].each_row do |row|
+            origin_keys = row.values_at(*origin_names)
+            @joins[origin_keys] ||= []
+            @joins[origin_keys].push(row.values_at(*target_names))
+          end
         end
       end
 
       def setup(dataset)
-        super
+        perform_setup(dataset) do |dataset|
+          other = Dataset.new
+          @target_reader.read(other)
 
-        other = Dataset.new
-        @target_reader.read(other)
-        other.each_field do |field|
-          if !@target_fields.include?(field.name)
-            dataset.add_field(field)
+          target_names = @target_fields.collect { |f| f[:name] }
+          other.each_field do |field|
+            if !target_names.include?(field.name)
+              dataset.add_field(field)
+            end
           end
+          dataset
         end
       end
 
       def transform(row)
-        row = super
+        perform_transform(row) do |row|
+          target_names = @target_fields.collect { |f| f[:name] }
 
-        origin_keys = row.values_at(*@origin_fields)
-        @target_reader.each_row do |merge_row|
-          target_keys = merge_row.values_at(*@target_fields)
-          if origin_keys == target_keys
-            row = row.merge(merge_row.reject { |k, v| @target_fields.include?(k) })
-            break
+          if @joins
+            origin_names = @origin_fields.collect { |f| f[:alias] }
+            origin_keys = row.values_at(*origin_names)
+            row_joins = @joins[origin_keys]
+            @target_reader.each_row do |target_row|
+              target_keys = target_row.values_at(*target_names)
+              if row_joins.include?(target_keys)
+                row = row.merge(target_row)
+                break
+              end
+            end
+          else
+            origin_names = @origin_fields.collect { |f| f[:name] }
+            origin_keys = row.values_at(*origin_names)
+            @target_reader.each_row do |target_row|
+              target_keys = target_row.values_at(*target_names)
+              if origin_keys == target_keys
+                row = row.merge(target_row.reject { |k, v| target_names.include?(k) })
+                break
+              end
+            end
           end
+          row
         end
+      end
 
-        row
+      private
+
+      def process_field(field)
+        case field
+        when String
+          { :name => field, :alias => field }
+        when Hash
+          field
+        end
       end
 
       register('merge', self)
--- test/unit/ethel/operations/test_merge.rb
+++ test/unit/ethel/operations/test_merge.rb
@@ -75,6 +75,42 @@ module TestOperations
         op.transform({'id' => 1, 'foo' => 'one'}))
     end
 
+    test "using a join table" do
+      target_reader = stub('target reader')
+
+      join_reader = stub('join reader')
+      join_row_1 = {'origin_id' => 1, 'target_id' => 2}
+      join_row_2 = {'origin_id' => 2, 'target_id' => 1}
+      join_reader.expects(:each_row).multiple_yields([join_row_1], [join_row_2]).once
+      op = Operations::Merge.new(target_reader, {
+        :join_reader => join_reader,
+        :origin_fields => [
+          { :name => 'id', :alias => 'origin_id' }
+        ],
+        :target_fields => [
+          { :name => 'fooid', :alias => 'target_id' }
+        ]
+      })
+
+      target_row_1 = {'fooid' => 1, 'bar' => 'one'}
+      target_row_2 = {'fooid' => 2, 'bar' => 'two'}
+      target_reader.expects(:each_row).multiple_yields([target_row_1], [target_row_2]).twice
+
+      origin_row_1 = {'id' => 1, 'foo' => 'one'}
+      expected_row_1 = {
+        'origin_id' => 1, 'foo' => 'one',
+        'target_id' => 2, 'bar' => 'two'
+      }
+      assert_equal(expected_row_1, op.transform(origin_row_1))
+
+      origin_row_2 = {'id' => 2, 'foo' => 'two'}
+      expected_row_2 = {
+        'origin_id' => 2, 'foo' => 'two',
+        'target_id' => 1, 'bar' => 'one'
+      }
+      assert_equal(expected_row_2, op.transform(origin_row_2))
+    end
+
     test "registers itself" do
       assert_equal Operations::Merge, Operation['merge']
     end
